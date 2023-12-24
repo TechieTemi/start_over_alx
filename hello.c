03e213c5879edfdc873f50f3ecbc9e06c7937fe0
TechieTemi <apparelreepublik@gmail.com>
2023-12-24 23:16:15 +0100

    My first hello world in starting over

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of a program
+ * Description: A program that print to stdout
+ * Return: Always(success)
+ */
+
+int main(void)
+{
+	printf("Hello World\n");
+	return(0);
+}
